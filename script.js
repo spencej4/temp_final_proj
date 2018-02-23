f85e5cd15f3f71da70eea434c093907fa0241377
spencej4 <32679180+spencej4@users.noreply.github.com>
2018-02-23 13:32:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-
-$(document).ready(function(){
+$(window).load(function() {
+    // Construct the query string
+    url = "https://data.kingcounty.gov/resource/gkhn-e8mn.json"
+          + '?zip_code=98107'; //hardcoded zipcode parameter...for now
+    
+    // Intialize our map
+    var center = new google.maps.LatLng(47.608013,-122.335167);
+    var mapOptions = {
+      zoom: 12,
+      center: center
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    
+    // Retrieve our data and plot it
+    $.getJSON(url, function(data, textstatus) {
+          $.each(data, function(i, entry) {
+              console.log(entry);
+              var marker = new google.maps.Marker({
+                  position: new google.maps.LatLng(entry.latitude, 
+                                                   entry.longitude),
+                  map: map,
+                  title: location.name
+              });
+          });
+    });
+});
+//------------------------------start previous code-------------------------------//
+/*
     $.ajax({
         url: "https://data.kingcounty.gov/resource/gkhn-e8mn.json",
         type: "GET",
@@ -22,7 +48,8 @@ function useReturnData(data){
         }
     }
 }
-
+*/
+//------------------------------end previous code---------------------------------//
 /*
 function contains(key, val) {
     		for (var i = 0; i < myData.length; i++) {
@@ -34,5 +61,5 @@ function contains(key, val) {
 }
 
 contains("zip_code","98107");
-*/
 });
+*/
