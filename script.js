ccdb0ffd76ae87b056edc37c86cd8863ada74735
Caitlyn Chung <caitlyn.chung@gmail.com>
2018-03-01 09:43:00 -0800

    New sidebar input form, build URL based on input form data

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 $(window).load(function() {
     // Construct the query string
     url = "https://data.kingcounty.gov/resource/gkhn-e8mn.json"
-          + '?zip_code=98107'; //hardcoded zipcode parameter...for now
+          + '?zip_code=98107&$limit=5'; //hardcoded zipcode parameter...for now, limit to 5 for now
+    
+    // This doesn't really do anything except show that a URL can be built using form inputs
+    $( "#inputForm" ).submit(function( event ) {
+      let inputZIP= $('#inputForm').find('input[name="searchZIP"]').val();
+      let inputViolation= $('#inputForm').find('input[name="searchViolation"]').val();
+      // make sure the key in searchParams is a valid field in the Food Safety database
+      let searchParams = {'zip_code': inputZIP,
+                          'violation_description': inputViolation
+                          };
+      let searchURL = generateURL(searchParams);
+      alert( searchURL ); //for testing
+    });
     
     // Intialize our map
     var center = new google.maps.LatLng(47.608013,-122.335167);
@@ -24,6 +36,20 @@ $(window).load(function() {
           });
     });
 });
+
+//this uses both the key and value strings to build the URL
+function generateURL(searchParams) {
+    let searchParamString = "";
+    for (let key in searchParams) {
+        // skip loop if the property is from prototype
+        if (!searchParams.hasOwnProperty(key)) continue;
+        searchParamString = searchParamString + key + "=" + searchParams[key] + "&"
+    }
+    // remove the last &
+    searchParamString = searchParamString.slice(0, -1); 
+    return ("https://data.kingcounty.gov/resource/gkhn-e8mn.json?"
+          + searchParamString);
+}
 //------------------------------start previous code-------------------------------//
 /*
     $.ajax({
