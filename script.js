3cee250b7f2abe4523ed90208898362c3bcb3840
spencej4 <32679180+spencej4@users.noreply.github.com>
2018-03-02 13:42:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,98 +1,60 @@
 let map; //The map variable that is an instance of a google.maps.Map is local to the initialize function. 
          //this forces global scope of map to be reused and initialized in further function calls
 
-
 $(window).load(function() {
-    // Construct the query string
-    url = "https://data.kingcounty.gov/resource/gkhn-e8mn.json"
-          + '?zip_code=98107'; //hardcoded zipcode parameter...for now
 
-    // Intialize our map
-    var center = new google.maps.LatLng(47.608013,-122.335167);
-    var mapOptions = {
-      zoom: 12,
-      center: center
+// Intialize our map
+  var center = new google.maps.LatLng(47.608013,-122.335167);
+  var mapOptions = {
+   zoom: 12,
+   center: center
+  }
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
+//this uses both the key and value strings to build the URL
+function generateURL(searchParams) {
+    let searchParamString = "";
+    for (let key in searchParams) {
+        // skip loop if the property is from prototype
+        if (!searchParams.hasOwnProperty(key)) continue;
+        searchParamString = searchParamString + key + "=" + searchParams[key] + "&"
     }
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    // remove the last &
+    searchParamString = searchParamString.slice(0, -1); 
+    return ("https://data.kingcounty.gov/resource/gkhn-e8mn.json?"
+          + searchParamString);
+  }
+
+
+// submit form
+$( "#inputForm" ).submit(function(e) {
+    e.preventDefault(); //added to prevent form submission from reloading the page
+    let inputZIP= $('#inputForm').find('input[name="searchZIP"]').val();
+    let inputGrade = $('#inputForm').find('input[name="grade"]').val();
+    // make sure the key in searchParams is a valid field in the Food Safety database
+    let searchParams = {'zip_code': inputZIP,
+                        'grade': inputGrade,
+                        };
+    searchURL = generateURL(searchParams);
+    //alert( searchURL ); //for testing
+
+
+    // Retrieve our data and plot it
+    $.getJSON(searchURL, function(data, textstatus) {
+        myData = data;
+      })
+        .done(function(){
+          //alert('ajax call performed'); //for testing
+            $.each(myData, function(i, entry) {
+               console.log(entry);
+               var marker = new google.maps.Marker({
+                   position: new google.maps.LatLng(entry.latitude, 
+                                                    entry.longitude),
+                   map: map,
+                   title: location.name
+               });
+           });
+        });    
+      });
 });
-
-
-let myData = null;
-
-$.ajax({
-    async: false,
-    url: "https://data.kingcounty.gov/resource/gkhn-e8mn.json",
-    type: "GET",
-    data: {
-      "$limit" : 5000,
-    }
-}).done(function(data) {
-  myData = data;
-  alert(`Retrieved ${data.length} records from the dataset!`);
-  return myData;
-});
-
-//when user submits a zipcode by clicking submit
-$('#zipcode_submit').click(function(){
-        //sets local variable to input field value
-        let zipcode = $('#zipcode_input').val();
-        //calls function and passes the variable as a parameter
-        findZipCode(zipcode);
-});
-
-//searches for all instances of zipcode value 
-function findZipCode(val) {
-        let numZipcodes = 0;
-        let zipCodeArr =[];
-        for (var i = 0; i < myData.length; i++) {
-            if(myData[i].zip_code === val){
-                //logs object instances which match
-                console.log(myData[i]);
-                numZipcodes += 1;
-                zipCodeArr.push(myData[i]);            }
-        }
-        alert(`Found ${numZipcodes} matching zipcodes from the dataset!`);
-        //console.log(zipCodeArr); //instead here we should be creating new google maps with the new array
-
-        $.each(zipCodeArr, function(i, entry) {
-              console.log(entry);
-              var marker = new google.maps.Marker({
-                  position: new google.maps.LatLng(entry.latitude, 
-                                                   entry.longitude),
-                  map: map,
-                  title: location.name
-              });
-          });
-}
-
-//when user submits an insepction score by clicking submit
-$('#inspection_score_submit').click(function(){
-        //sets local variable to input field value
-        let score = $('#inspection_score_input').val();
-        //calls function and passes the variable as a parameter
-        findScore(score);
-});
-
-//searches for all instances of inspection score value 
-function findScore(val) {
-        let numInspScores =0;
-        for (var i = 0; i < myData.length; i++) {
-            if(myData[i].inspection_score === val){
-                //logs object instances which match
-                console.log(myData[i]);
-                numInspScores += 1;
-            }
-        }
-        alert(`Found ${numInspScores} matching inspections scores from the dataset!`);
-}
-
-//extra
-//generic function to search data objects keys and values
-function findMatch(key, val) {
-        alert("running findMatch");
-        for (var i = 0; i < myData.length; i++) {
-            if(myData[i][key] === val){
-                console.log(myData[i]);
-            }
-        }
-}
